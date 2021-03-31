3fd2399f000ef66dfd59dc27b47be4eebfb9099d
remopas <remopas@hotmail.nl>
2021-03-31 16:56:43 +0200

    added db config

---
+++ config/db.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+	HOST: 'localhost',
+	USER: 'remopas',
+	PASSWORD: '',
+	DB: 'skate-map',
+	dialect: 'postgresql'
+}
--- models/index.js
+++ models/index.js
@@ -1,6 +1,11 @@
 
+const dbConfig = require('../config/db.config.js')
+
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize('postgres://remopas:@localhost:5432/skate-map');
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+	host: dbConfig.HOST,
+	dialect: dbConfig.dialect
+});
 
 models = ['location']
 models.forEach(m => {
