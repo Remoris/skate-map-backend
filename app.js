312a7b823c0fd435bc1d5b8fa31d7f0eed486851
remopas <remopas@hotmail.nl>
2021-04-07 18:34:14 +0200

    docker

--- app.js
+++ app.js
@@ -5,32 +5,40 @@ const logger = require('morgan');
 
 const db = require('./models')
 
-db.authenticate()
-	.then(() => console.log('database connection established'))
-	.catch((e) => console.error('database connection failed', e));
-
 const locations = require('./placeholder.json').locations
 
-db.sync({force: true}).then(() => {
+const indexRouter = require('./routes/index.js');
+const apiRouter = require('./routes/api.router.js');
+
+const setupApp = () => {
+	const app = express();
+	app.use(logger('dev'));
+	app.use(express.json());
+	app.use(express.urlencoded({ extended: false }));
+	app.use(cookieParser());
+	app.use(express.static(path.join(__dirname, 'public')));
+	app.use('/', indexRouter);
+	app.use('/api/v1', apiRouter);
+	return app
+}
+
+const connectDB = (retry = true) => db.authenticate()
+	.then(() => {
+		console.log('database connection established')
+		setupDB()
+	})
+	.catch(e => {	
+		console.error('database connection failed', e)
+		if(retry){ setTimeout(connectDB, 5000) }
+	})
+
+const setupDB = () => db.sync({force: true}).then(() => {
 	locations.forEach(l => {
 		l.coords = db.Sequelize.fn('ST_MakePoint', l.coords.longitude, l.coords.latitude)
 		db.models.Location.create(l)
 	})
 })
 
+connectDB();
 
-const indexRouter = require('./routes/index.js');
-const apiRouter = require('./routes/api.router.js');
-
-const app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/api/v1', apiRouter);
-
-module.exports = app;
+module.exports = setupApp();
