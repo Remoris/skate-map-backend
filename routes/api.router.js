bca08367e43d3dfa584bf7b685344dbc96aca065
Remo Pas <remopas@hotmail.nl>
2022-01-21 20:39:23 +0100

    django initial

--- routes/api.router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const locationRouter = require('./location.router.js')
-
-router.use('/locations', locationRouter)
-
-module.exports = router;
